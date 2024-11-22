024c301fede7d0722aee08db11eee5992252a183
busiterapp@gmail.com <busiterapp@gmail.com>
2024-11-22 09:00:09 +0100

    resubida  david2

--- src/utils/calculations/transportation/nightSurcharge/index.ts
+++ src/utils/calculations/transportation/nightSurcharge/index.ts
@@ -1,6 +1,5 @@
 // src/utils/calculation/calculateNightSurcharge.ts
 export const calculateNightSurcharge = (serviceHours: number, hoursPerDriver: number, nightDriverSurcharge: number) => {
     return serviceHours > hoursPerDriver ? nightDriverSurcharge : 0;
-    
 };
   
