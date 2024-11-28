398b33c45e40499aa85138f22d9cac382ba6c678
Alvaro E. Ricotta <alvaro.e.ricotta@gmail.com>
2024-11-28 06:01:27 +0100

    fixing round 6

--- src/utils/services/transportation/getIdleDriver.ts
+++ src/utils/services/transportation/getIdleDriver.ts
@@ -16,40 +16,36 @@ type IdleProps = {
 };
 
 export function getIdleDriver({ company, formData, drivers, breakdown }: IdleProps): number {
-    // la primera parte incluye tramo nocturno?
-    const Segment1NightIncluded = breakdown.some((breaks:any) => breaks.segment === 'departure');
+    const Segment1NightIncluded = breakdown.some((breaks: any) => breaks.segment === 'departure');
 
     const pricePerDriver = company.overnight_supplement_per_driver;
 
     const { startDate, startTime, endDate, endTime } = formData;
-    const startDateTime = bt(startDate,startTime);
-    const endDateTime = bt(endDate,endTime);
+    const startDateTime = bt(startDate, startTime);
+    const endDateTime = bt(endDate, endTime);
 
-    const nextDayNightEndTime = bt(startDate,company.night_end_time).add(1, 'day');
+    const nextDayNightEndTime = bt(startDate, company.night_end_time).add(1, 'day');
 
     if (!startDateTime.isValid() || !endDateTime.isValid()) {
         console.error('Fechas de inicio o fin no son válidas');
         return 0;
     }
 
-    const hoursDifference = endDateTime.diff(startDateTime, 'hour', true);
+    const minutesDifference = endDateTime.diff(startDateTime, 'minute');
     const driverMultiplier = drivers > 0 ? 2 : 1;
 
-    // Verificar si la finalización es posterior al night_end_time del día siguiente
+    const fullDays = Math.floor(minutesDifference / 1440); // 1440 minutos = 24 horas
+    const remainingMinutes = minutesDifference % 1440;
+
+    const daysDifference = fullDays + (remainingMinutes > 480 ? 1 : 0);
+
     if (!Segment1NightIncluded && endDateTime.isAfter(nextDayNightEndTime)) {
-        // Si la hora de finalización es después del night_end_time, se aplicará el recargo
-        const daysDifference = Math.ceil(hoursDifference / 24);
         const price = daysDifference * pricePerDriver;
         return price * driverMultiplier;
-    } else if (Segment1NightIncluded && hoursDifference > 8) {
-        // Si el segmento incluye una noche y la diferencia de horas es mayor a 8, se calcula el recargo
-        const daysDifference = Math.ceil(hoursDifference / 24);
+    } else if (Segment1NightIncluded && minutesDifference > 480) {
         const price = daysDifference * pricePerDriver;
         return price * driverMultiplier;
     }
 
     return 0;
 }
-
-
-
